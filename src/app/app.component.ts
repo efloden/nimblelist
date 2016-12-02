3d0276ebc9d3b561c4facc2338597ff537f8dd97
Earl Floden <earl.mark.27@gmail.com>
2016-12-02 16:47:10 +1000

    Shopping list Model implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
+import { ITEMS } from './mocks';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'NimbleList : Groceries';
+  budget = 100;
+  items: Item[];
+
+  ngOnInit() {
+      this.items = ITEMS;
+  }
+
+  totalCost() {
+    let sum = 0;
+    for (let item of this.items) {
+      if (item.buying) sum += item.cost;
+    }
+    return sum;
+  }
+
+  cashLeft() {
+    return this.budget - this.totalCost();
+  }
 }
---
+++ src/app/mocks.ts
@@ -0,0 +1,18 @@
+import { Item } from './item';
+
+export const ITEMS: Item[] = [{
+  "id": 1,
+  "name": "Milk",
+  "cost": 2,
+  "buying": true
+}, {
+  "id": 2,
+  "name": "Eggs",
+  "cost": 3.2,
+  "buying": true
+}, {
+  "id": 3,
+  "name": "Cereal",
+  "cost": 4.5,
+  "buying": true
+}];
